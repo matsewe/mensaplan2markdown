4aade0466cea6c89b56870089c1eae3437da2e8b
mweber <matthias.weber@uni-ulm.de>
2018-04-25 15:46:08 +0200

    Correct json

--- index.php
+++ index.php
@@ -7,17 +7,17 @@ $escapeChars = array("|", "\"");
 
 header('Content-Type: application/json');
 
-echo "{\"response_type\": \"in_channel\", \"text\": \"\\\n";
+echo "{\"response_type\": \"in_channel\", \"text\": \"";
 #echo "---";
 #echo "#### Essen in der Mensa am ".date("d. m. Y")."\n";
-echo "| Mensa | ".$date." |\\\n";
-echo "|:------|:----------|\\\n";
+echo "| Mensa | ".$date." |\\n";
+echo "|:------|:----------|\\n";
 
 foreach($json->weeks as $week => $wdata) {
     foreach($wdata->days as $day => $ddata) {
         if ($ddata->date == $date) {
             foreach($ddata->Mensa->meals as $mealId => $meal) {
-                echo "| ".str_replace($escapeChars, '', $meal->category)." | ".str_replace($escapeChars, '', $meal->meal)." |\\\n";
+                echo "| ".str_replace($escapeChars, '', $meal->category)." | ".str_replace($escapeChars, '', $meal->meal)." |\\n";
             }
         }
     }
